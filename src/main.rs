1f61cb075a4f8af4d7adec2e0e4ad0d310747642
Tony Finch <dot@dotat.at>
2021-07-26 16:57:18 +0100

    simplify

--- src/main.rs
+++ src/main.rs
@@ -30,16 +30,20 @@ const SWITCH_HOLE: f64 = 14.0 / MM_IN - KERF;
 const COUNT_WIDTH: u8 = 4;
 const COUNT_DEPTH: u8 = 4;
 
+const PCB_LEFT: f64 = OUT_SIDE + INNER;
+const PCB_FAR: f64 = OUT_FAR + INNER;
 const PCB_WIDTH: f64 = (COUNT_WIDTH as f64) * SWU;
 const PCB_DEPTH: f64 = (COUNT_DEPTH as f64) * SWU;
 
-const PCB_LEFT: f64 = OUT_SIDE + INNER;
-const PCB_FAR: f64 = OUT_FAR + INNER;
-
 const PCB_GAP: f64 = BLUNT;
 
-const IN_RIGHT: f64 = OUT_SIDE + PCB_WIDTH + 2.0 * INNER;
-const IN_NEAR: f64 = OUT_FAR + PCB_DEPTH + 2.0 * INNER;
+const BOX_LEFT: f64 = PCB_LEFT - PCB_GAP;
+const BOX_FAR: f64 = PCB_FAR - PCB_GAP;
+const BOX_WIDTH: f64 = PCB_WIDTH + PCB_GAP * 2.0;
+const BOX_DEPTH: f64 = PCB_DEPTH + PCB_GAP * 2.0;
+
+const IN_WIDTH: f64 = PCB_WIDTH + 2.0 * INNER;
+const IN_DEPTH: f64 = PCB_DEPTH + 2.0 * INNER;
 
 const WIDTH: f64 = (OUT_SIDE + INNER) * 2.0 + PCB_WIDTH;
 const DEPTH: f64 = OUT_FAR + OUT_NEAR + INNER * 2.0 + PCB_DEPTH;
@@ -57,75 +61,64 @@ impl Path {
         Path { data: self.data.close() }
     }
 
-    fn outer(self) -> Path {
+    fn move_to(self, x: f64, y: f64) -> Path {
+        Path { data: self.data.move_to((x, y)) }
+    }
+
+    fn open_rect(self, width: f64, depth: f64) -> Path {
         Path {
             data: self
                 .data
-                .move_to((CORNER, 0.0))
-                .elliptical_arc_by((CORNER, CORNER, 0, 0, 0, -CORNER, CORNER))
-                .line_to((0.0, DEPTH - CORNER))
-                .elliptical_arc_by((CORNER, CORNER, 0, 0, 0, CORNER, CORNER))
-                .line_to((WIDTH - CORNER, DEPTH))
-                .elliptical_arc_by((CORNER, CORNER, 0, 0, 0, CORNER, -CORNER))
-                .line_to((WIDTH, CORNER))
-                .elliptical_arc_by((CORNER, CORNER, 0, 0, 0, -CORNER, -CORNER)),
+                .line_by((0.0, depth))
+                .line_by((width, 0.0))
+                .line_by((0.0, -depth)),
         }
     }
 
-    fn surround_inner(self) -> Path {
+    fn rounded_rect(self, width: f64, depth: f64, radius: f64) -> Path {
         Path {
             data: self
                 .data
-                .move_to((OUT_SIDE + BLUNT, OUT_FAR))
-                .elliptical_arc_by((BLUNT, BLUNT, 0, 0, 0, -BLUNT, BLUNT))
-                .line_to((OUT_SIDE, IN_NEAR - BLUNT))
-                .elliptical_arc_by((BLUNT, BLUNT, 0, 0, 0, BLUNT, BLUNT))
-                .line_to((IN_RIGHT - BLUNT, IN_NEAR))
-                .elliptical_arc_by((BLUNT, BLUNT, 0, 0, 0, BLUNT, -BLUNT))
-                .line_to((IN_RIGHT, OUT_FAR + BLUNT))
-                .elliptical_arc_by((BLUNT, BLUNT, 0, 0, 0, -BLUNT, -BLUNT))
-                .close(),
+                .move_by((radius, 0.0))
+                .elliptical_arc_by((radius, radius, 0, 0, 0, -radius, radius))
+                .line_by((0.0, depth - radius * 2.0))
+                .elliptical_arc_by((radius, radius, 0, 0, 0, radius, radius))
+                .line_by((width - radius * 2.0, 0.0))
+                .elliptical_arc_by((radius, radius, 0, 0, 0, radius, -radius))
+                .line_by((0.0, -depth + radius * 2.0))
+                .elliptical_arc_by((radius, radius, 0, 0, 0, -radius, -radius)),
         }
+    }
+
+    fn outer(self) -> Path {
+        self.move_to(0.0, 0.0).rounded_rect(WIDTH, DEPTH, CORNER)
+    }
+
+    fn surround_holder(self) -> Path {
+        self.move_to(OUT_SIDE, OUT_FAR).open_rect(IN_WIDTH, IN_DEPTH).close()
     }
 
     fn surround_outer(self) -> Path {
-        Path {
-            data: self
-                .data
-                .move_to((OUT_SIDE, OUT_FAR))
-                .line_to((OUT_SIDE, IN_NEAR))
-                .line_to((IN_RIGHT, IN_NEAR))
-                .line_to((IN_RIGHT, OUT_FAR))
-                .close(),
-        }
+        self.move_to(OUT_SIDE, OUT_FAR)
+            .rounded_rect(IN_WIDTH, IN_DEPTH, BLUNT)
+            .close()
     }
 
-    fn inner(self) -> Path {
-        Path {
-            data: self
-                .data
-                .move_to((PCB_LEFT, PCB_FAR))
-                .line_to((PCB_LEFT, PCB_FAR + PCB_DEPTH))
-                .line_to((PCB_LEFT + PCB_WIDTH, PCB_FAR + PCB_DEPTH))
-                .line_to((PCB_LEFT + PCB_WIDTH, PCB_FAR))
-                .close(),
-        }
+    fn surround_inner(self) -> Path {
+        self.move_to(PCB_LEFT, PCB_FAR).open_rect(PCB_WIDTH, PCB_DEPTH).close()
+    }
+
+    fn pcb_box(self) -> Path {
+        self.open_rect(BOX_WIDTH, BOX_DEPTH)
     }
 
     fn switch_hole(self, x: f64, y: f64, w: f64) -> Path {
-        let left = PCB_LEFT + (x + w / 2.0) * SWU - SWITCH_HOLE / 2.0;
-        let far = PCB_FAR + (y + 0.5) * SWU - SWITCH_HOLE / 2.0;
-        let right = left + SWITCH_HOLE;
-        let near = far + SWITCH_HOLE;
-        Path {
-            data: self
-                .data
-                .move_to((left, far))
-                .line_to((left, near))
-                .line_to((right, near))
-                .line_to((right, far))
-                .close(),
-        }
+        self.move_to(
+            PCB_LEFT + (x + w / 2.0) * SWU - SWITCH_HOLE / 2.0,
+            PCB_FAR + (y + 0.5) * SWU - SWITCH_HOLE / 2.0,
+        )
+        .open_rect(SWITCH_HOLE, SWITCH_HOLE)
+        .close()
     }
 
     fn cut(self) -> svg::node::element::Path {
@@ -144,7 +137,7 @@ fn main() -> Result<()> {
             path = path.switch_hole(x as f64, y as f64, 1.0);
         }
     }
-    path = path.inner().surround_outer().outer().close();
+    path = path.surround_inner().surround_outer().outer().close();
 
     let size = (-SWU, -SWU, WIDTH + SWU * 2.0, DEPTH + SWU * 2.0);
 
