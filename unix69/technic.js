922d828ef6cab8218558b2911388c904db49ef4d
Tony Finch <dot@dotat.at>
2023-06-26 18:19:12 +0100

    draw fasteners and left socket

--- unix69/technic.js
+++ unix69/technic.js
@@ -29,14 +29,18 @@ function main() {
     c.lineWidth = 0.5;
     c.transform(2, 0, 0, 2, canvas.width/2, canvas.height/2);
 
-    let stud = 20;
+    let key_unit = 19.05; // mm
+    let lego_unit = 0.4; // mm
+
+    let stud = 20; // ldu
     let beam_x = 394;
     let beam_y = 132;
 
-    beam(c, +6 * stud, -beam_y, 9, 1);
-    beam(c, -6 * stud, -beam_y, 9, 1);
-    beam(c, +15 * stud - 1, -beam_y, 9, 1);
-    beam(c, -15 * stud + 1, -beam_y, 9, 1);
+    beam(c, +4 * stud, -beam_y, 5, 1);
+    beam(c, -4 * stud, -beam_y, 5, 1);
+    beam(c, +13 * stud - 1, -beam_y, 13, 1);
+    beam(c, -11 * stud + 1, -beam_y, 9, 1);
+    beam(c, -19 * stud + 1, -beam_y, 1, 1);
     beam(c, +13 * stud - 1, +beam_y, 13, 1);
     beam(c, -13 * stud + 1, +beam_y, 13, 1);
     beam(c, 0, +beam_y, 13, 1);
@@ -54,8 +58,48 @@ function main() {
     roundrect(c, -plate_width, -plate_depth,
 	      plate_width * 2, plate_depth * 2, plate_radius);
 
+    let bolt_radius = 4;
+    let nut_radius = 6;
+
+    circle(c, +beam_x, +6 * stud, bolt_radius);
+    circle(c, +beam_x, -6 * stud, bolt_radius);
+    circle(c, -beam_x, +6 * stud, bolt_radius);
+    circle(c, -beam_x, -6 * stud, bolt_radius);
+
+    circle(c, +beam_x, +6 * stud, nut_radius);
+    circle(c, +beam_x, -6 * stud, nut_radius);
+    circle(c, -beam_x, +6 * stud, nut_radius);
+    circle(c, -beam_x, -6 * stud, nut_radius);
+
+    circle(c, +7 * stud - 1, +beam_y, bolt_radius);
+    circle(c, -7 * stud + 1, +beam_y, bolt_radius);
+    circle(c, +7 * stud - 1, -beam_y, bolt_radius);
+    circle(c, -7 * stud + 1, -beam_y, bolt_radius);
+
+    circle(c, +7 * stud - 1, +beam_y, nut_radius);
+    circle(c, -7 * stud + 1, +beam_y, nut_radius);
+    circle(c, +7 * stud - 1, -beam_y, nut_radius);
+    circle(c, -7 * stud + 1, -beam_y, nut_radius);
+
+    circle(c, +15 * stud - 1, -beam_y, nut_radius);
+    circle(c, -15 * stud + 1, -beam_y, nut_radius);
+
+    circle(c, +19 * stud - 1, +beam_y, nut_radius);
+    circle(c, -19 * stud + 1, +beam_y, nut_radius);
+    circle(c, +19 * stud - 1, -beam_y, nut_radius);
+    circle(c, -19 * stud + 1, -beam_y, nut_radius);
+
+    circle(c, +6 * stud, +beam_y, nut_radius);
+    circle(c, -6 * stud, +beam_y, nut_radius);
+    circle(c, +6 * stud, -beam_y, nut_radius);
+    circle(c, -6 * stud, -beam_y, nut_radius);
+
+    circle(c, +2 * stud, -beam_y, nut_radius);
+    circle(c, -2 * stud, -beam_y, nut_radius);
+
+    let plug_width = 36; // with extra clearance
     let socket_width = 24;
-    let socket_depth = 24;
+    let socket_depth = 30;
     let socket_overhang = 6;
     let socket_recess = 5;
     c.strokeRect(-socket_width/2, -plate_depth + socket_recess,
@@ -67,4 +111,14 @@ function main() {
     c.lineTo(+18, -plate_depth + socket_overhang + socket_recess);
     c.lineTo(+36, -pcb_depth);
     c.stroke();
+
+    let left_socket = -7 * key_unit/lego_unit;
+    c.strokeRect(left_socket - socket_width/2, -pcb_depth - socket_overhang,
+		 socket_width, socket_depth);
+
+    c.setLineDash([1,2]);
+
+    c.strokeRect(left_socket - plug_width / 2, -plate_depth,
+		 plug_width, plate_depth - pcb_depth + socket_depth);
+
 }
