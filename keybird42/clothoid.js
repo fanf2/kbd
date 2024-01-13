a27cba2d2025c9726d67dcfef37f21a2f511ebea
Tony Finch <dot@dotat.at>
2024-01-13 21:18:30 +0000

    use integer index for position along clothoid

--- keybird42/clothoid.js
+++ keybird42/clothoid.js
@@ -9,7 +9,9 @@ const sqrt = Math.sqrt;
 function main() {
     let c = canvas.getContext("2d");
     c.translate(canvas.width/2, canvas.height/2);
-    c.scale(1000, 1000);
+    let scale = 1000;
+    c.scale(scale, scale);
+    c.rotate(tau*2/3);
 
     function style(w, s, f) {
 	c.lineWidth = w;
@@ -29,17 +31,16 @@ function main() {
     // y = ∫ sin(s²) ds
 
     function clothoid(fun) {
-	let s = 0;
+	let ds = 4/scale; // integration step size
 	let x = 0;
 	let y = 0;
-	for (;;) {
-	    let ds = 0.01; // integration step size
+	for (let i = 0; i < 8/ds; i++) {
+	    let s = i * ds;
 	    let dx = cos(s*s) * ds;
 	    let dy = sin(s*s) * ds;
-	    if (fun(s, x, y, dx, dy)) {
+	    if (fun(i, x, y, dx, dy)) {
 		return;
 	    }
-	    s += ds;
 	    x += dx;
 	    y += dy;
 	}
@@ -54,13 +55,13 @@ function main() {
 	return (sqrt(x*x + y*y));
     }
 
-    function inside(x, y) {
-	return (length(lim_c - x, lim_c - y) < lim_r);
+    function limit(x, y) {
+	return (length(x - lim_c, y - lim_c) < lim_r);
     }
 
     // adjust according to aesthetic preference
-    function width(s) {
-	return (0.1 / (s + 0.5));
+    function width(i) {
+	return (25 / (i + 125));
     }
 
     style(0.01, "#008", "#0000");
@@ -69,46 +70,47 @@ function main() {
     style(0.01, "#800", "#0000");
     circle(+lim_c, +lim_c, lim_r);
     circle(-lim_c, -lim_c, lim_r);
+    circle(+lim_c, +lim_c, 0.01);
+    circle(-lim_c, -lim_c, 0.01);
 
-    function inner(mirror) {
-	style(0.005, "#000", "#0000");
-	c.beginPath();
-	c.moveTo(0, -width(0) * mirror);
-	clothoid(function(s, x, y, dx, dy) {
-	    let w = width(s) / length(dx, dy);
-	    let xx = x + dy * w;
-	    let yy = y - dx * w;
-	    c.lineTo(mirror * xx, mirror * yy);
-	    return (inside(xx, yy));
-	});
-	c.stroke();
-    }
-
-    function outer(mirror) {
-	style(0.005, "#000", "#0000");
-	c.beginPath();
-	c.moveTo(0, +width(0) * mirror);
-	clothoid(function(s, x, y, dx, dy) {
-	    let w = width(s) / length(dx, dy);
-	    let xx = x - dy * w;
-	    let yy = y + dx * w;
-	    c.lineTo(mirror * xx, mirror * yy);
-	    return (inside(xx, yy));
-	});
-	c.stroke();
-    }
-
-    clothoid(function(s, x, y, dx, dy) {
-	style(width(s) * 2, "#8888", "#0000");
+    clothoid(function(i, x, y, dx, dy) {
+	style(width(i) * 2, "#8888", "#0000");
 	c.beginPath();
 	c.moveTo(+x,    +y);
 	c.lineTo(+x+dx, +y+dy);
 	c.moveTo(-x,    -y);
 	c.lineTo(-x-dx, -y-dy);
 	c.stroke();
-	return (inside(x, y));
     });
 
-    outer(+1); inner(+1);
-    outer(-1); inner(-1);
+    function outline(inner, mirror) {
+	style(0.005, "#000", "#0000");
+	c.beginPath();
+	c.moveTo(0, width(0) * inner * mirror);
+	clothoid(function(i, x, y, dx, dy) {
+	    let w = inner * width(i) / length(dx, dy);
+	    let xx = x - dy * w;
+	    let yy = y + dx * w;
+	    c.lineTo(mirror * xx, mirror * yy);
+	    return (limit(xx, yy));
+	});
+	c.stroke();
+    }
+
+    for (let inner of [-1, +1]) {
+	for (let mirror of [-1, +1]) {
+	    outline(inner, mirror);
+	}
+    }
+
+    // find out where inner curve meets the limit
+    let xe = 0;
+    let ye = 0;
+    clothoid(function(i, x, y, dx, dy) {
+	let w = width(i) / length(dx, dy);
+	xe = x + dy * w;
+	ye = y - dx * w;
+	return (limit(xe, ye));
+    });
+    console.log(xe, ye);
 }
