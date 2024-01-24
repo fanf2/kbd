fc2f602a563bc480aaf6f49b969115145115f745
Tony Finch <dot@dotat.at>
2024-01-24 20:43:58 +0000

    more bending
    the plate sides are too short for laserboost so i need a rethink...

--- keybird42/case_bends.py
+++ keybird42/case_bends.py
@@ -23,23 +23,44 @@ def ArcFrom(curve, pos, radius, angle):
     return JernArc(curve @ pos, tangent(curve, pos),
                    radius, interpolated(pos) * angle)
 
-thick = MX_PLATE_THICK
+TYPING_ANGLE = 7
 
-inner_radius = thick # required by laserboost
-outer_radius = inner_radius + thick
+TOTAL_WIDTH = ku(24)
+
+CHIN_DEPTH = ku(1)
+BROW_DEPTH = ku(1)
+FOREHEAD_DEPTH = ku(3)
+
+THICK = MX_PLATE_THICK
+
+inner_radius = THICK # required by laserboost
+outer_radius = inner_radius + THICK
 radius = (inner_radius + outer_radius) / 2
+
+# minimum metal needed eash side of a bend
+bend_grip = 7.1 - THICK - inner_radius
 
 plate_to_top = 7
 pcb_to_plate = 5
 
 pcba_thick = 4 # MX pin length plus clearance
 
+washer_thick = 1
+side_inset = 1
+
+plate_width = TOTAL_WIDTH - 2 * (
+    inner_radius + THICK + washer_thick + THICK + side_inset)
+
+# need to work out the proper details here...
+plate_depth = KEYS_DEPTH + (CHIN_DEPTH + BROW_DEPTH) / 2
+
 # plate, side-to-side
 
-plate_side_height = plate_to_top - inner_radius - thick
+plate_side_height = plate_to_top - inner_radius - THICK
 
-plate_x_axis = Line((-KEYS_WIDTH/2, 0),
-                    (+KEYS_WIDTH/2, 0))
+
+plate_x_axis = Line((-plate_width/2, 0),
+                    (+plate_width/2, 0))
 
 right_arc = ArcFrom(plate_x_axis, 1, radius, 90)
 right_side = LineFrom(right_arc, 1, plate_side_height)
@@ -48,34 +69,34 @@ left_arc = ArcFrom(plate_x_axis, 0, radius, 90)
 left_side = LineFrom(left_arc, 1, plate_side_height)
 
 plate_sides = (
-    Location((0, KEYS_DEPTH/2, thick/2)) * Rotation(X=90) *
+    Location((0, plate_depth/2, THICK/2)) * Rotation(X=90) *
     extrude(trace(
         left_side + left_arc + plate_x_axis + right_arc + right_side,
-        thick), amount=KEYS_DEPTH))
+        THICK), amount=plate_depth))
 
 # plate, front-to-rear
 
 plate_lip_height = pcba_thick + pcb_to_plate - inner_radius
 
-plate_y_axis = Line((0, -KEYS_DEPTH/2),
-                    (0, +KEYS_DEPTH/2))
+plate_y_axis = Line((0, -plate_depth/2),
+                    (0, +plate_depth/2))
 
-rear_arc = ArcFrom(plate_y_axis, 1, radius, 90)
+rear_arc = ArcFrom(plate_y_axis, 1, radius, 90 - TYPING_ANGLE)
 rear_lip = LineFrom(rear_arc, 1, plate_lip_height)
 
-front_arc = ArcFrom(plate_y_axis, 0, radius, 90)
+front_arc = ArcFrom(plate_y_axis, 0, radius, 90 - TYPING_ANGLE)
 front_lip = LineFrom(front_arc, 1, plate_lip_height)
 
 plate_lips = (
-    Location((KEYS_WIDTH/2, 0, thick/2)) * Rotation(Y=-90) *
+    Location((plate_width/2, 0, THICK/2)) * Rotation(Y=-90) *
     extrude(trace(
         rear_lip + rear_arc + plate_y_axis + front_arc + front_lip,
-        thick), amount=KEYS_WIDTH))
+        THICK), amount=plate_width))
 
 
 # combined plate
 
-plate_cutouts = extrude(keyswitch_cutouts(), amount=thick)
+plate_cutouts = extrude(keyswitch_cutouts(), amount=THICK)
 
 plate = plate_sides + plate_lips - plate_cutouts
 
