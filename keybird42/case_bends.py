da859536a9b1d292560d32c5e8328268ab4cf5d6
Tony Finch <dot@dotat.at>
2024-01-25 14:33:49 +0000

    some of the outer enclosure

--- keybird42/case_bends.py
+++ keybird42/case_bends.py
@@ -13,18 +13,12 @@ def print_object(message, object):
 def LineBy(start, direction, length):
     return Line(start, start + (length / direction.length) * direction)
 
-TYPING_ANGLE_DIVISOR = 48
-TYPING_ANGLE_DEGREES = 360 / TYPING_ANGLE_DIVISOR
-TYPING_ANGLE_RADIANS = math.tau / TYPING_ANGLE_DIVISOR
+def sweep_arc(face, radius, angle, plane=Plane.XY):
+    return sweep(face, JernArc(
+        face.center(), face.normal_at(), radius, angle, plane=plane))
 
-CHIN_DEPTH = ku(1)
-BROW_DEPTH = ku(1)
-FOREHEAD_DEPTH = ku(3)
-
-TOTAL_WIDTH = ku(24)
-
-# provisional
-TOTAL_DEPTH = CHIN_DEPTH + KEYS_DEPTH + BROW_DEPTH + FOREHEAD_DEPTH
+def sweep_line(face, length):
+    return sweep(face, LineBy(face.center(), face.normal_at(), length))
 
 THICK = MX_PLATE_THICK
 
@@ -37,7 +31,7 @@ outer_radius = inner_radius + THICK
 BEND_RADIUS = (inner_radius + outer_radius) / 2
 
 # minimum metal needed eash side of a bend
-bend_grip = 7.1 - THICK - inner_radius
+bend_grip = 7.1 - outer_radius
 
 washer_thick = 1.5
 
@@ -48,10 +42,22 @@ pcba_thick = 4 # MX pin length plus clearance
 plate_to_top = magnet_thick + washer_thick + THICK
 pcb_to_plate = 5
 
-side_inset = THICK / 3 # for appearance
+base_inset = THICK / 3 # for appearance
+base_clear = THICK / 3
 
 # this also determines the top cutout radius
 keycap_clearance = 0.5
+
+TYPING_ANGLE_DIVISOR = 48
+TYPING_ANGLE_DEGREES = 360 / TYPING_ANGLE_DIVISOR
+TYPING_ANGLE_RADIANS = math.tau / TYPING_ANGLE_DIVISOR
+
+CHIN_DEPTH = ku(1)
+BROW_DEPTH = ku(2)
+
+TOTAL_WIDTH = ku(24)
+TOP_DEPTH = CHIN_DEPTH + KEYS_DEPTH + BROW_DEPTH
+TOP_Y = BROW_DEPTH/2 - CHIN_DEPTH/2
 
 plate_surround = ku(0.25)
 
@@ -61,18 +67,13 @@ assert plate_surround + MX_PLATE_RIB/2 > bend_grip
 
 datum_y = KEYS_DEPTH / 2
 datum_z = pcba_thick + pcb_to_plate
-
-datum_loc = (Location((0, 0, datum_z)) *
-             Rotation(X=TYPING_ANGLE_DEGREES) *
-             Location((0, datum_y, 0)))
+datum_loc = (Rotation(X=TYPING_ANGLE_DEGREES) *
+             Location((0, datum_y, datum_z)))
 
 # all of these are centred on the middle of the key blocks
 
 plate_width = KEYS_WIDTH + plate_surround*2
 plate_depth = KEYS_DEPTH + plate_surround*2
-
-top_width = TOTAL_WIDTH
-top_depth = CHIN_DEPTH + KEYS_DEPTH + BROW_DEPTH
 
 plate_cutouts = thick(keyswitch_cutouts())
 plate = datum_loc * (thick(Rectangle(plate_width, plate_depth)) - plate_cutouts)
@@ -81,22 +82,41 @@ top_sharpcut = thick(offset(
     keycap_cutouts(), amount=keycap_clearance, kind=Kind.INTERSECTION))
 top_cutouts = fillet(top_sharpcut.edges() | Axis.Z, keycap_clearance)
 top = datum_loc * Location((0,0,plate_to_top)) * (
-    thick(Rectangle(top_width, top_depth)) - top_cutouts)
+    thick(Location((0,TOP_Y)) * Rectangle(TOTAL_WIDTH, TOP_DEPTH))
+    - top_cutouts)
 
 pcb = datum_loc * Location((0,0,-pcb_to_plate)) * kb42_pcba()
 
 show_object(plate)
-show_object(top)
 show_object(pcb)
 
-plate_front = plate.faces().sort_by(Axis.Y)[0]
+top_front = top.faces().sort_by(Axis.Y)[0]
+chin_arc = sweep_arc(
+    top_front, BEND_RADIUS, +90 - TYPING_ANGLE_DEGREES, plane=Plane.YZ)
+chin_top = chin_arc.faces().sort_by(Axis.Z)[0]
+front_wall = sweep_line(chin_top, chin_top.center().Z + THICK)
 
-arc = sweep(plate_front,
-            JernArc(plate_front.center(), plate_front.normal_at(),
-                    BEND_RADIUS, 90 - TYPING_ANGLE_DEGREES, plane=Plane.YZ))
+top_rear = top.faces().sort_by(Axis.Y)[-1]
+brow_arc = sweep_arc(
+    top_rear, BEND_RADIUS, -90 - TYPING_ANGLE_DEGREES, plane=Plane.YZ)
+brow_top = brow_arc.faces().sort_by(Axis.Z)[0]
+rear_wall = sweep_line(brow_top, brow_top.center().Z + THICK)
 
-arc_bottom = arc.faces().sort_by(Axis.Z)[0]
+top += [ chin_arc, front_wall, brow_arc, rear_wall ]
 
-wall = sweep(arc_bottom, LineBy(arc_bottom.center(), arc_bottom.normal_at(), arc_bottom.center().Z))
+top = fillet(top.edges() << Axis.Z | Axis.Y, outer_radius)
 
-show_object(arc + wall)
+show_object(top)
+
+[base_front, base_rear] = [
+    face.center() for face in top.faces() << Axis.Z > Axis.Y ]
+
+base_front = base_front.Y + THICK/2 + base_clear
+base_rear = base_rear.Y - THICK/2 - base_clear
+base_depth = base_rear - base_front
+base_width = TOTAL_WIDTH - base_inset*2 - outer_radius*2
+base_y = base_rear/2 + base_front/2
+
+base = Location((0,base_y)) * thick(Rectangle(base_width, base_depth))
+
+show_object(base)
