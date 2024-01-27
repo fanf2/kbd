61add839728d14fbc1f4e705a87a448da6ff62eb
Tony Finch <dot@dotat.at>
2024-01-27 20:17:35 +0000

    gradually working towards the right race of the board

--- keybird42/case_curvy.py
+++ keybird42/case_curvy.py
@@ -17,8 +17,9 @@ THICK = 1.5
 KEYCAP_CLEAR = 0.5
 
 # defining features
-FRONT_RADIUS = ku(1)/2
-REAR_RADIUS = ku(2)/2
+FRONT_HEIGHT = 7+5+4+1
+FRONT_RADIUS = FRONT_HEIGHT/2
+REAR_RADIUS = FRONT_RADIUS*2
 
 TOTAL_WIDTH = ku(24)
 TOTAL_DEPTH = TOTAL_WIDTH/3
@@ -33,9 +34,11 @@ ELLIPSE_Y_RADIUS = MIDDLE_DEPTH/2
 MAIN_Y = MAIN_DEPTH/2 - MIDDLE_DEPTH/2 + ku(0.5)
 
 half_angle = atan2((REAR_RADIUS - FRONT_RADIUS), MIDDLE_DEPTH)
+typing_angle = half_angle*2
 
-print(f"{MIDDLE_DEPTH=}")
-print(f"{half_angle=}")
+print(f"{FRONT_HEIGHT=}")
+print(f"{MIDDLE_DEPTH/KEY_UNIT=}")
+print(f"{typing_angle=}")
 
 top = Rotation(X=+half_angle) * Line(
     (0, 0, +FRONT_RADIUS),
@@ -53,8 +56,8 @@ rear_y = IntersectingLine(top.location_at(1).position,
 print(f"{rear_y=}")
 
 front = ThreePointArc(top.vertices().sort_by(Axis.Y)[+0],
-                  (0, -FRONT_RADIUS, 0),
-                  base.vertices().sort_by(Axis.Y)[+0])
+                      (0, -FRONT_RADIUS, 0),
+                      base.vertices().sort_by(Axis.Y)[+0])
 
 rear = ThreePointArc(top.vertices().sort_by(Axis.Y)[-1],
                      (0, rear_y, 0),
@@ -68,13 +71,47 @@ section = make_face([top,base,front,rear])
 
 middle = extrude(section, amount=MIDDLE_WIDTH/2, both=True)
 
-top_face = middle.faces().sort_by(Axis.Z)[-1]
+top_center = middle.faces().sort_by(Axis.Z)[-1].center_location
 
-# for some reason the top face's XY plane is a YX plane
-keyholes = (top_face.center_location *
-            (Rotation(Z=-90) *
-             Location((0,MAIN_Y,0)) *
-             extrude(keyswitch_cutouts(),
-                     amount=-REAR_RADIUS*2)))
+def on_top(shape):
+    # for some reason the top face's XY plane is a YX plane
+    return top_center * (Rotation(Z=-90) * shape)
 
-show_object(middle - keyholes)
+holes = on_top(Location((0,MAIN_Y,0)) * (
+    extrude(keycap_cutouts(), amount=-7) +
+    extrude(keyswitch_cutouts(), amount=-7-5)))
+
+show_object(middle - holes)
+
+whole_top = on_top(Rectangle(TOTAL_WIDTH, TOTAL_DEPTH))
+
+right_ellipse = on_top(Location((MIDDLE_WIDTH/2, 0)) *
+                       Ellipse(ELLIPSE_X_RADIUS, ELLIPSE_Y_RADIUS))
+
+right_top_edge = (right_ellipse & whole_top).edges().sort_by(Axis.Z)[-1]
+right_base_edge = (Location(top_center.to_axis().direction * -FRONT_HEIGHT)
+                   * right_top_edge)
+
+right_base_front = right_base_edge @ 0
+right_base_rear = right_base_edge @ 1
+right_top_front = right_top_edge @ 0
+right_top_rear = right_top_edge @ 1
+
+right_front_center = (right_top_front + right_base_front) / 2
+right_rear_center = (right_top_rear + right_base_rear) / 2
+right_center = (right_front_center + right_rear_center) / 2
+
+right_front_circle = (Location(right_front_center)
+                      * (Plane.YZ * Circle(FRONT_RADIUS)))
+right_rear_circle = (Location(right_rear_center)
+                     * (Plane.YZ * Circle(FRONT_RADIUS)))
+
+right_rectangle = Polygon(right_top_front,
+                          right_top_rear,
+                          right_base_rear,
+                          right_base_front)
+
+right_face = (Location(right_center - right_rectangle.center())
+              * right_rectangle) + right_front_circle + right_rear_circle
+
+show_object(right_face)
