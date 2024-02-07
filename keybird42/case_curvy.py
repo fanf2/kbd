ffc0db848c40d286fa802c378ad5462bd1f59127
Tony Finch <dot@dotat.at>
2024-02-07 21:17:40 +0000

    liking this version so far

--- keybird42/case_curvy.py
+++ keybird42/case_curvy.py
@@ -27,11 +27,12 @@ keys_depth = ku(5)
 
 keys_y0 = main_y - keys_depth/2
 
+front_r = ku(0.5)
 front_x = ku(4)
 front_y = keys_y0 - ku(0.5)
-front_r = ku(0.5)
 
-centre_y = front_y - front_r + total_depth/2
+min_y = front_y - front_r
+centre_y = min_y + total_depth/2
 
 side_r = ku(1)
 side_x = total_width/2 - side_r
@@ -40,7 +41,6 @@ side_w = ku(1/3)
 rear_r = ku(1.0)
 rear_x = ku(6)
 rear_y = front_y - front_r + ku(8) - rear_r
-rear_z = front_r - rear_r
 
 corner_x = side_x - side_w
 corner_y = keys_y0 + ku(1/3)
@@ -57,11 +57,9 @@ show_marker((side_x, 0))
 
 show_object(Location((0, main_y)) * keycap_cutouts(), **rgba("3333"))
 
-show_object(Location((0, centre_y, front_r))
+show_object(Location((0, centre_y))
             * Rectangle(total_width, total_depth),
             **rgba("cccc"))
-
-
 
 # find ellipse radii given displacement
 # from point on axis to point on diagonal
@@ -99,55 +97,42 @@ def side_ellipse():
     centre_x = side_x - radius_x
     centre_y = 0
     return EllipticalCenterArc(
-        (centre_x, centre_y), radius_x, radius_y, -90, 0
-    ) & RectangleAt(corner_x, corner_y, width, height)
+        (centre_x, centre_y), radius_x, radius_y, -90, +90
+    ) & RectangleAt(corner_x, corner_y, width, height*2)
 
-def front_curve():
-    path = (Line((0, front_y), (front_x, front_y))
-            + front_ellipse()
-            + side_ellipse())
+def curve_section(path, t):
+    pos = path @ t
+    rot = -Vector(0,1,0).get_signed_angle(path % t)
+    normal = (path % t).rotate(Axis.Z, -90)
 
-    section = make_face(ThreePointArc(
-        (0, front_y, +front_r),
-        (0, front_y - front_r, 0),
-        (0, front_y, -front_r)
-    ) + Line(
-        (0, front_y, -front_r),
-        (0, front_y, +front_r)
-    ))
+    yt = (pos.Y - front_y) / (rear_y - front_y)
+    z = front_r + yt * (rear_r - front_r)
 
-    squashed_path = scale(path, (1/side_stretch, 1, 1))
-    squashed_curve = sweep(section, squashed_path.edges())
+    xt = (pos.X - front_x) / (side_x - front_x)
+    if pos.Y >= 0: xt = 1
+    xtt = max(0,xt)**6
+    stamp((xt, xtt))
+    r = min(front_r + xtt * (rear_r - front_r),
+            pos.Y - min_y)
 
-    return scale(squashed_curve, (side_stretch, 1, 1))
-
-def smoothstep(t):
-    return 3 * t**2 - 2 * t**3
-
-def rear_section(path, t):
-    eps = 0.2
-    # the cad kernel gets into trouble if we try to specify the curve too precisely
-    #s = 0 if t < eps else smoothstep((t - eps) / (1 - eps))
-    s = smoothstep(t)
-    z = (0, 0, s * rear_z)
-    r = front_r - s * rear_z
-    pos = path @ t + z
-    rot = Vector(0,1,0).get_angle(path % t)
     show_object(Location(pos) * Box(t+1,t+1,t+1))
+    show_object(Line(pos, pos + normal * r))
+    show_object(Line(pos, pos - (0, 0, 2*z)))
+
     semi = Plane.XZ * make_face(EllipticalCenterArc(
-        (0,0), ku(1.0), r, -90, +90
-    ) + Line((0,-r), (0,+r)))
-    return Location(pos) * Rotation(Z=rot) * semi
+        (0,0), r, z, -90, +90
+    ) + Line((0,-z), (0,+z)))
+    return Location(pos - (0,0,z)) * Rotation(Z=rot) * semi
 
 def rear_curve():
-    side_path = mirror(side_ellipse(), Plane.ZX).edges()
-    corner_path = rear_ellipse()
+    path = front_ellipse() + side_ellipse() + rear_ellipse()
 
     # the cad kernel goes AWOL if this is cranked up too much
-    steps = 4
-    sections = [ rear_section(corner_path, i/steps)
+    steps = 32
+    sections = [ curve_section(path, i/steps)
                  for i in range(steps+1) ]
     show_object(sections)
+    return
     stamp("sections")
 
     start_pos = (0, rear_y, rear_z)
@@ -156,17 +141,14 @@ def rear_curve():
     show_object(rear_curve, **rgba("4444"))
     stamp("rear")
 
-    side_curve = sweep(sections[0], side_path)
-    show_object(side_curve, **rgba("4444"))
-    stamp("side")
+    # side_curve = sweep(sections[0], side_path)
+    # show_object(side_curve, **rgba("4444"))
+    # stamp("side")
 
     corner_curve = sweep(sections, corner_path, multisection=True)
     show_object(corner_curve, **rgba("4444"))
     stamp("corner")
 
     return
-
-show_object(front_curve(), **rgba("4444"))
-stamp("front")
 
 rear_curve()
