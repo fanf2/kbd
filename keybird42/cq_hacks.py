10daa96aa41e412c01097b8de9cae214a9371228
Tony Finch <dot@dotat.at>
2024-02-07 13:16:48 +0000

    growing a library of helper functions

--- keybird42/cq_hacks.py
+++ keybird42/cq_hacks.py
@@ -1,6 +1,53 @@
+from build123d import *
 import inspect
 import math
 import OCP.Aspect
+import time
+
+# surely there's a better way?
+show_object = None
+for f in inspect.stack():
+    if "show_object" in f.frame.f_locals:
+        show_object = f.frame.f_locals["show_object"]
+
+START = time.perf_counter()
+def stamp(msg):
+    t = time.strftime('%F.%T')
+    z = time.strftime('%z')
+    f = (time.time_ns() // 100000) % 10000
+    print(f"{t}.{f:03}{z} {time.perf_counter() - START :6.3f} {msg}")
+
+def topo(obj):
+    try: return obj.show_topology()
+    except: return f"{obj}"
+
+def show_normal_tangent(edge, pos):
+    point = edge @ pos
+    tangent = edge % pos * 10
+    for angle in [0, 90, 180]:
+        show_object(Line(point, point + tangent.rotate(Axis.Z, angle)))
+
+def show_marker(p):
+    show_object(Location(p) * Box(1,1,1), **rgba("f00"))
+
+# for use with show_object()
+def rgba(rgba):
+    color = ()
+    if len(rgba) < 5:
+        for i in range(3):
+            color += (int(rgba[i:i+1], 16) * 17,)
+    else:
+        for i in range(3):
+            color += (int(rgba[i*2:i*2+2], 16),)
+    alpha = None
+    if len(rgba) == 4:
+        alpha = int(rgba[-1:], 16) / 15.0
+    if len(rgba) == 8:
+        alpha = int(rgba[-2:], 16) / 255.0
+    options = { "color": color }
+    if alpha != None:
+        options["alpha"] = alpha
+    return { "options": options }
 
 # surely there's a better way?
 def find_main_window():
