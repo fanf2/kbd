cfc4a0c969b1dff32b3e1c4b18382cb0d4f4805c
Tony Finch <dot@dotat.at>
2024-02-11 16:28:35 +0000

    looking plausible

--- keybird42/superellipse-bezier.py
+++ keybird42/superellipse-bezier.py
@@ -17,11 +17,11 @@ def superpoint(a, b, e, theta):
 def dpow(u, e):
     return e * pow(abs(u), e-1)
 
-def dsuperpoint(a, b, e, theta):
+def supertangent(a, b, e, theta):
     ( ct, st ) = ( cos(theta), sin(theta) )
-    return (( 0, 1 ) if theta == 0 else
-            ( a * -st * dpow(ct, 2/e),
-              b * +ct * dpow(st, 2/e) ))
+    return (Vector( 0, 1 ) if theta == 0 else
+            Vector( a * -st * dpow(ct, 2/e),
+                    b * +ct * dpow(st, 2/e) ).normalized())
 
 def superellipse_spline(a, b, e, n):
     points = [ superpoint(a, b, e, d * (tau/4))
@@ -37,6 +37,17 @@ def superellipse_lines(a, b, e, n):
         superpoint(a, b, e, d * tau)
         for d in distances(n)
     ], close=True))
+
+def triangle_apex(p0, t0, p1, t1):
+    target_line = Line(p1, p1 - t1 * (p1-p0).length)
+    return IntersectingLine(p0, t0, target_line) @ 1
+
+def superellipse_bezier(a, b, e, n):
+    pt = [ (Vector(superpoint(a, b, e, d * tau)),
+            supertangent(a, b, e, d * tau)) for d in distances(n) ]
+    curves = [ Bezier(p0, triangle_apex(p0, t0, p1, t1), p1)
+               for ((p0,t0),(p1,t1)) in zip(pt, pt[1:] + pt[:1]) ]
+    return make_face(curves)
 
 def arrow(shaft):
     return Arrow(arrow_size = shaft.length/4, head_at_start = False,
@@ -60,7 +71,9 @@ def show_superellipse(fun, Q, n):
                 options={"color": (255 - 51*Q,)*3})
     return squircle
 
-show_superellipse(superellipse_lines, 2, 32)
+show_superellipse(superellipse_lines, 3, 32)
+
+show_superellipse(superellipse_bezier, 2, 32)
 
 splined = show_superellipse(superellipse_spline, 1, 32)
 show_object([ normal_ray(splined.wire(), d, ray)
@@ -68,8 +81,9 @@ show_object([ normal_ray(splined.wire(), d, ray)
 
 def super_arrow(a, b, e, theta):
     p = superpoint(a, b, e, theta)
-    t = Vector(dsuperpoint(a, b, e, theta)).normalized().rotate(Axis.Z, -90)
-    return arrow(Line(p, p+t*ray))
+    t = supertangent(a, b, e, theta)
+    n = t.rotate(Axis.Z, -90)
+    return arrow(Line(p, p+n*ray))
 
 show_object([ super_arrow(width, height, expo, d*tau)
                for d in distances(32) ])
