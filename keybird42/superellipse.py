e2944e41849e5a5325c8150207fc194ea0d6a9fe
Tony Finch <dot@dotat.at>
2024-02-12 19:54:36 +0000

    broken wip better superellipses

--- keybird42/superellipse.py
+++ keybird42/superellipse.py
@@ -1,51 +1,147 @@
+"""superellipses and superellipsoids
+
+There are two complementary definitions of a superellipse, depending
+on whether the author prefers [the implicit equation][enwp] or [the
+parametric equation][bourke] (scroll down past the sportsball
+section). In the implicit version, superellipses are ellipses when
+`e = 2` and get more rectangular (hyperelliptical) for larger `e`,
+and more pointy (hypoelliptical) for smaller `e`. In the parametric
+version, superellipses are rectangles at `e = 0`, ellipses at `e =
+1`, and pointy for larger `e`.
+
+[enwp]: https://en.wikipedia.org/wiki/Superellipse
+[bourke]: https://paulbourke.net/geometry/spherical/
+
+This code uses the parametric form to draw hyperellipses with
+exponents less than 1.
+
+In the same way that ellipses are scaled circles, superellipses are
+scaled squircles. Therefore this code only constructs squircles; the
+result must be scaled to get a superellipse. Similarly in three
+dimensions for superellipsoids.
+
+We approximate a superellipse by dividing it into sectors. The
+superellipse's parameter `theta` ranges from 0 to `tau`; each sector
+is an equal subdivision of this range. (But not an equal subdivision
+of the resulting superellipse owing to its exponent.)
+
+We can calculate the tangents at the points between each sector of
+the superellipse. These determine a quadratic Bézier for each
+segment. We don't have to use too many segments to get a good enough
+approximation to the superellipse.
+
+"""
+
 from build123d import *
-from cq_hacks import stamp
+from collections import namedtuple
+from cq_hacks import *
 from functools import cache
-from itertools import pairwise
 from math import cos, sin, tau
 
-RESOLUTION = 16
+# Default number of sectors per quadrant is DETAIL+2
+DETAIL=3
 
-def triangle_apex(p0, t0, p1, t1):
-    target_line = Line(p1, p1 - t1 * (p1-p0).length)
-    return IntersectingLine(p0, t0, target_line) @ 1
-
-def vertex_ray(e1, e0, length):
-    assert e1 @ 1 == e0 @ 0
-    point = (e1 @ 1 + e0 @ 0) / 2
-    tangent = (e1 % 1 + e0 % 0) / 2
-    normal = tangent.rotate(Axis.Z, -90)
-    return Line(point, point + normal * length)
-
-def angles(n):
-    return [ tau * i / n for i in range(n) ]
+# The clamp here is helpful for testing
+def power(u, e):
+    return max(2**-256, abs(u)) ** e
 
 def sgn(a):
     return (-1 if a < 0 else
             +1 if a > 0 else 0)
 
-def power(u, e):
-    return max(2**-256, abs(u)) ** e
+def mul(a, b):
+    return a.X * b.Y - a.Y * b.X
 
-def superpoint(a, b, e, r):
-    (c, s) = (cos(r), sin(r))
-    return Vector(a * sgn(c) * power(c, 2/e),
-                  b * sgn(s) * power(s, 2/e))
 
-def supertangent(a, b, e, r):
-    (c, s) = (cos(r), sin(r))
-    return Vector(a * -s * power(c, 2/e - 1),
-                  b * +c * power(s, 2/e - 1)).normalized()
+# The Bézier curves are too flat near the axes where there's a
+# relatively large gap between points. The approximation is a lot
+# closer to a superellipse if we add intermediate points in these
+# gaps; empirically, 0.25 either side of each axis produces a more
+# even spacing of points and good results as low as `DETAIL=3`
+#
+# Note this list excludes both endpoints, which are handled as special
+# cases to ensure that superellipses are properly closed.
+@cache
+def quadrant_angles(n):
+    return [ (tau / 4) * (i / n)
+             for i in [0.25] + list(range(1, n)) + [n-0.25] ]
 
-def superellipse(a, b, e, resolution=RESOLUTION):
-    pt = [ (superpoint(a, b, e, r),
-            supertangent(a, b, e, r))
-           for r in angles(resolution) ]
-    return make_face([
-        Bezier(p0, triangle_apex(p0, t0, p1, t1), p1)
-        for ((p0, t0), (p1, t1))
-        in zip(pt, pt[1:] + pt[:1]) ])
+# We want to make a quadratic Bézier curve from p0 to p1,
+# with tangents t0 at p0 and t1 at p1. Its control point
+# is at the intersection of the tangent lines.
+def bezier_ctrl(p0, t0, p1, t1):
+    (pt0, pt1) = (p0 + t0, p1 + t1)
+    div = mul(t0, t1)
+    m0 = mul(p0, pt0) / div
+    m1 = mul(p1, pt1) / div
+    return Vector(m1 * t0.X - m0 * t1.X,
+                  m1 * t0.Y - m0 * t1.Y)
 
+def superpoint(e, 𝜃):
+    (c, s) = (cos(𝜃), sin(𝜃))
+    return Vector(sgn(c) * power(c, e),
+                  sgn(s) * power(s, e))
+
+def supertangent(e, 𝜃):
+    (c, s) = (cos(𝜃), sin(𝜃))
+    return Vector(-s * power(c, e - 1),
+                  +c * power(s, e - 1))
+
+# points on X and Y axes are special cases
+def superquadrant(e, detail=DETAIL):
+    pt = [ (Vector(1,0), Vector(0,1)) ] + [
+        (superpoint(e, 𝜃), supertangent(e, 𝜃))
+        for 𝜃 in quadrant_angles(detail)
+    ] +  [ (Vector(0,1), Vector(-1,0)) ]
+    return [ Bezier(p0, bezier_ctrl(p0, t0, p1, t1), p1)
+             for ((p0,t0),(p1,t1)) in zip(pt[:-1], pt[1:]) ]
+
+def superellipse(e, detail=DETAIL):
+    quarter = superquadrant(e, detail)
+    half = mirror(quarter, Plane.YZ) + quarter
+    whole = mirror(half, Plane.ZX) + half
+    result = make_face(whole)
+    return result
+
+# for testing and experimentation
+if __name__ != 'superellipse':
+
+    for e in range(10):
+        E = (e + 1) / 10
+        stamp(f"{E=}")
+        R = 20 - e
+
+        # piecewise linear version to compare accuracy of curves
+        HIRES=256
+        show_object(Pos((0,0,e*3+1)) * extrude(make_face(Polyline(*[
+            R * superpoint(E, tau*i/HIRES) for i in range(HIRES)
+        ], close=True)), 1), **rgba("aaa"))
+
+        show_object(Pos((0,0,e*3))
+                    * extrude(scale(superellipse(E), R), 1),
+                    **rgba("666"))
+
+        # go round the whole way instead of just one quarter
+        quarter = [0] + quadrant_angles(DETAIL)
+        theta = [ a + q * tau/4 for q in range(4) for a in quarter ]
+
+        # break out the construction
+        show = []
+        for a0, a1 in zip(theta, theta[1:] + theta[:1]):
+            p0 = R * superpoint(E, a0)
+            p1 = R * superpoint(E, a1)
+            t0 = supertangent(E, a0)
+            t1 = supertangent(E, a1)
+            cp = bezier_ctrl(p0, t0, p1, t1)
+            show += [ Pos(cp) * Box(.1,.1,.1), Bezier(p0, cp, p1),
+                      Line(p0, cp), Line(cp, p1), Line(p1, p0),
+                      arrow(Line(p0, p0+t0.normalized())),
+                      arrow(Line(p1, p1-t1.normalized())) ]
+        show_object([ Pos((0,0,e*3+2)) * thing for thing in show ])
+
+"""
+
+@cache
 def superellipse_half(a, b, e, resolution=RESOLUTION):
     clip = Location((0, b/2)) * Rectangle(a*2, b)
     return superellipse(a, b, e, resolution) & clip
@@ -75,5 +171,6 @@ def superellipsoid(inner, outer, h, e, resolution=RESOLUTION):
         for i,e in enumerate(sectors) ]
 
 def superegg_half(a, b, e, resolution=RESOLUTION):
-    """Half a superellipse revolved halfway around the X axis"""
     return revolve(superellipse_half(a, b, e, resolution), Axis.X, 180)
+
+"""
